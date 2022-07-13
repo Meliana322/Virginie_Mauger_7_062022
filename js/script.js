5fa1d4c85ffa8ffdc56582b50ffd929aec3258d1
Mauger <meliana.322@gmail.com>
2022-07-13 16:39:57 +0200

    remove tags

--- js/script.js
+++ js/script.js
@@ -34,17 +34,20 @@ let arrayTags = [];
 
 function temp() {
   const listDropdowns = document.querySelectorAll(".list-dropdown li");
-  const listTags = document.querySelectorAll(".tag-list");
-  console.log(listTags);
+  // const listTags = document.querySelectorAll(".tag-list");
+  // console.log(listTags);
   listDropdowns.forEach((el) => {
     // Pour chaque élément de la liste de tags
     el.addEventListener("click", (e) => {
       // Au click sur un élément
       const tagElement = e.target; // La cible
       console.log(tagElement);
+      // Ajoute la classe "click" à l'élément de la liste ciblé
       tagElement.classList.add("click");
 
+      // Affiche les tags
       displayTags(tagElement);
+
       arrayTags.push({
         category: tagElement.parentNode.id,
         value: tagElement.textContent,
@@ -52,32 +55,37 @@ function temp() {
       resultFilterTag(filteredRecipe, arrayTags);
       temp();
     });
+    // ! REMOVE TAGS
+    console.log(arrayTags);
+    const closeBtnArray = [...document.querySelectorAll(".close-tag")];
+
+    closeBtnArray.forEach((closeBtn) => {
+      closeBtn.addEventListener("click", (e) => {
+        const tagsContainer = e.target.parentNode.parentNode;
+        const textTag = e.target.parentNode.parentNode.textContent;
+        console.log(textTag);
+        tagsContainer.remove();
+        console.log(arrayTags);
+        // supprimer le bon tag dans 'arrayTags'
+
+        arrayTags = arrayTags.filter(
+          (tagObj) => tagObj.value !== tagsContainer.textContent
+        );
+
+        // relancer le rendu "resultFilterTag"
+
+        resultFilterTag(filteredRecipe, arrayTags);
+        temp();
+      });
+    });
   });
 }
 // /////////////////////////////////////////////////
-// Affiche les tags dans le container
-const listDropdowns = document.querySelectorAll(".list-dropdown li");
-const listTags = document.querySelectorAll(".tag-list");
-console.log(listTags);
+// const listTags = document.querySelectorAll(".tag-list");
+// console.log(listTags);
 temp();
-// listDropdowns.forEach((el) => {
-//   // Pour chaque élément de la liste de tags
-//   el.addEventListener("click", (e) => {
-//     // Au click sur un élément
-//     const tagElement = e.target; // La cible
-//     console.log(tagElement);
-//     tagElement.classList.add("click");
 
-//     displayTags(tagElement);
-//     arrayTags.push({
-//       category: tagElement.parentNode.id,
-//       value: tagElement.textContent,
-//     });
-//     resultFilterTag(filteredRecipe, arrayTags);
-//   });
-// });
 // /////////////////////////////////////////////////
-// displayTags(recipes, arrayTags); // Affichage / Fermeture des tags et des recettes en fonction des choix utilisateur
 
 const recipesContainer = document.querySelector(".recipes-container");
 
